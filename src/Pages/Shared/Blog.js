bf2e849040b1d9a373f437999eaf46a23b0fa158
Imtiaz Ahmed Chowdhury <imtiazemon625@gmail.com>
2022-05-26 21:41:46 +0600

    Faq3

--- src/Pages/Shared/Blog.js
+++ src/Pages/Shared/Blog.js
@@ -62,24 +62,26 @@ URL state
         </li>
         <li class="w-2/5">
             <p class="text-lg font-medium leading-6 text-gray-900">
-                Does building an extension need an energy rating?
+            You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?
             </p>
             <p class="mt-2">
                 <p class="text-base leading-6 text-gray-500">
-                    Depended on the size of the extension you are building there is a chance that it too will need to be energy rated. It&#x27;s always best to check first before going ahead with construction.
+                Create two extra space, i.e. two extra arrays to store the product of all the array elements from start, up to that index and another array to store the product of all the array elements from the end of the array to that index. 
+To get the product excluding that index, multiply the prefix product up to index i-1 with the suffix product up to index i+1.
+
+Algorithm: 
+
+Create two array prefix and suffix of length n, i.e length of the original array, initialize prefix[0] = 1 and suffix[n-1] = 1 and also another array to store the product.
+Traverse the array from second index to end.
+For every index i update prefix[i] as prefix[i] = prefix[i-1] * array[i-1], i.e store the product upto i-1 index from the start of array.
+Traverse the array from second last index to start.
+For every index i update suffix[i] as suffix[i] = suffix[i+1] * array[i+1], i.e store the product upto i+1 index from the end of array
+Traverse the array from start to end.
+For every index i the output will be prefix[i] * suffix[i], the product of the array element except that element.
                 </p>
             </p>
         </li>
-        <li class="w-2/5">
-            <p class="text-lg font-medium leading-6 text-gray-900">
-                What is the general cost of an energy rating?
-            </p>
-            <p class="mt-2">
-                <p class="text-base leading-6 text-gray-500">
-                    Depended on the size of the extension you are building there is a chance that it too will need to be energy rated. It&#x27;s always best to check first before going ahead with construction.
-                </p>
-            </p>
-        </li>
+        
     </ul>
 </div>
 
