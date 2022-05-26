124a3bd8c19671f58a17a6b57f949be123eec66a
Imtiaz Ahmed Chowdhury <imtiazemon625@gmail.com>
2022-05-26 20:17:51 +0600

    Footer Details

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 import Banner from './Banner';
 import Services from './Services';
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Services></Services>
+            <Footer></Footer>
         </div>
     );
 };
